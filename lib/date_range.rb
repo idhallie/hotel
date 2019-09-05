47679aa9ba143d4cc8f8b10db432f21f60ccfd4c
Hallie <idhallie@gmail.com>
2019-09-05 16:17:32 -0700

    DateRange class raises an error if start_date and end_date are not entered correctly.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -4,5 +4,9 @@ class DateRange
   def initialize(start_date:, end_date:)
     @start_date = start_date
     @end_date = end_date
+    
+    unless start_date.instance_of?(Date) && end_date.instance_of?(Date)
+      raise ArgumentError.new("Dates must be entered as Date.new(yyyy, mm, dd). Got start date: #{start_date} and end_date: #{end_date}.")
+    end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -10,5 +10,23 @@ describe "DateRange" do
     it "is an instance of DateRange" do
       expect(@new_date).must_be_kind_of DateRange
     end
+    
+    it "requires that start_date must be an instance of Date class" do
+      expect(@new_date.start_date).must_be_kind_of Date
+    end
+    
+    it "requires that end_date must be an instance of Date class" do
+      expect(@new_date.start_date).must_be_kind_of Date
+    end
+    
+    it "raises an error if start_date is not a Date" do
+      expect { DateRange.new(start_date: "blerg", end_date: Date.new(2019, 10, 06))
+      }.must_raise ArgumentError
+    end
+    
+    it "raises an error if end_date is not a Date" do
+      expect { DateRange.new(start_date: Date.new(2019, 10, 03), end_date: "blorb")
+      }.must_raise ArgumentError
+    end
   end
 end
